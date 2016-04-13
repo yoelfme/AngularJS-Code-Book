1fac1ff9a6f17a559a8221a3d347a977f9fd0b08
Yoel Monzon <yoelfme@hotmail.com>
2016-04-12 23:58:48 -0600

    [add] sample app using yeoman

---
+++ chapter3/sample-app/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sampleAppApp
+ */
+angular.module('sampleAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ chapter3/sample-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sampleAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sampleAppApp
+ */
+angular.module('sampleAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
