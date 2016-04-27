0bce58cc9ab753307413bf9406da0001c848b4ba
Yoel Monzon <yoelfme@hotmail.com>
2016-04-27 00:25:14 -0600

    [add] bootstrapand app file to require js example

---
+++ chapter3/requirejsexample/app/scripts/app.js
@@ -0,0 +1,4 @@
+define(['angular', 'angularREsource', 'controllers/controllers',
+  'services/services'. 'filter/filters', 'directives/directives'], function (angular) {
+    return angular.module('MyApp', ['ngResource', 'controllers', 'services', 'filters', 'directives']);
+  });
---
+++ chapter3/requirejsexample/app/scripts/bootstrap.js
@@ -0,0 +1,5 @@
+define(['angular', 'domReady'], function (angular, domReady) {
+  domReady(function () {
+    angular.bootstrap(document, ['MyApp']);
+  });
+});
